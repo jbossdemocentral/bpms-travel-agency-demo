a93a1cd0d7ba3addb2066e2e3d9a75cbae510001
Eric D. Schabell <eric@schabell.org>
2015-03-05 10:44:49 +0000

    Fixed brackets for ubuntu.

--- init.sh
+++ init.sh
@@ -42,7 +42,7 @@ echo
 command -v mvn -q >/dev/null 2>&1 || { echo >&2 "Maven is required but not installed yet... aborting."; exit 1; }
 
 # make some checks first before proceeding.	
-if [[ -r $SRC_DIR/$BPMS || -L $SRC_DIR/$BPMS ]]; then
+if [ -r $SRC_DIR/$BPMS ] || [ -L $SRC_DIR/$BPMS ]; then
 		echo Product sources are present...
 		echo
 else
