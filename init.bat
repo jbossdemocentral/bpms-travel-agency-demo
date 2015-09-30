bddd96998acc1bae93b84581df23acfefcce67ec
Eric D. Schabell <eric@schabell.org>
2015-09-30 15:18:39 -0400

    Updated branch for coming 6.2 product release.

--- init.bat
+++ init.bat
@@ -14,9 +14,9 @@ set SERVER_BIN=%JBOSS_HOME%\bin
 set SRC_DIR=%PROJECT_HOME%installs
 set SUPPORT_DIR=%PROJECT_HOME%support
 set PRJ_DIR=%PROJECT_HOME%projects
-set BPMS=jboss-bpmsuite-6.1.0.GA-installer.jar
-set EAP=jboss-eap-6.4.0-installer.jar
-set VERSION=6.1
+set BPMS=jboss-bpmsuite-6.2.0.GA-installer.jar
+set EAP=jboss-eap-6.4.3-installer.jar
+set VERSION=6.2
 
 REM wipe screen.
 cls
--- init.sh
+++ init.sh
@@ -11,9 +11,9 @@ SERVER_BIN=$JBOSS_HOME/bin
 SRC_DIR=./installs
 SUPPORT_DIR=./support
 PRJ_DIR=./projects
-BPMS=jboss-bpmsuite-6.1.0.GA-installer.jar
-EAP=jboss-eap-6.4.0-installer.jar
-VERSION=6.1
+BPMS=jboss-bpmsuite-6.2.0.GA-installer.jar
+EAP=jboss-eap-6.4.3-installer.jar
+VERSION=6.2
 
 
 # wipe screen.
